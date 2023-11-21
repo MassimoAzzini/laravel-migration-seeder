e8be8f7a80e3cce090e30687d4681cfbc63a93df
MassimoAzzini <massimo.azzini.web@gmail.com>
2023-11-21 16:06:35 +0100

    [edit] db-time

--- database/migrations/2023_11_21_141239_create_trains_table.php
+++ database/migrations/2023_11_21_141239_create_trains_table.php
@@ -18,8 +18,8 @@ return new class extends Migration
             $table->string('Azienda', 50);
             $table->string('Stazione_di_partenza', 50);
             $table->string('Stazione_di_arrivo', 50);
-            $table->time('Orario_di_partenza');
-            $table->time('Orario_di_arrivo');
+            $table->dateTime('Partenza');
+            $table->dateTime('Arrivo');
             $table->smallInteger('Codice_treno')->unsigned();
             $table->tinyInteger('Numero_carrozze')->unsigned();
             $table->boolean('In_orario')->default(true);
