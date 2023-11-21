c87854b4bf6eeb939d24e4d2d50929409ec78af2
MassimoAzzini <massimo.azzini.web@gmail.com>
2023-11-21 15:36:42 +0100

    [add] db-table-trains

---
+++ database/migrations/2023_11_21_141239_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('Azienda', 50);
+            $table->string('Stazione_di_partenza', 50);
+            $table->string('Stazione_di_arrivo', 50);
+            $table->time('Orario_di_partenza');
+            $table->time('Orario_di_arrivo');
+            $table->smallInteger('Codice_treno')->unsigned();
+            $table->tinyInteger('Numero_carrozze')->unsigned();
+            $table->boolean('In_orario')->default(true);
+            $table->boolean('Cancellato')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main')
+
+@section('content')
+
+    <h1>HOME</h1>
+
+@endsection
